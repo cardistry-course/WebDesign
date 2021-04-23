8e4eb29115e200d0b143720981784ed191c30fe5
cardistry-course <samuraj550@gmail.com>
2021-04-23 19:43:03 +0200

    my first shere XD

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const toggle = document.querySelector('.toggle')
+const banner = document.querySelector('.banner')
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    banner.classList.toggle('active');
+}
+
+
+
+function paralax (e){
+    this.querySelectorAll('.elements').forEach(layer => {
+        const speed = layer.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX*speed)/100;
+        layer.style.transform = `translateX(${x}px)`
+    })
+}
+
+document.addEventListener("mousemove", paralax)
